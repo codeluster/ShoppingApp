f692665794d3484ff063b57931410c5130bd57b2
tanmay <tanmaysingal@icloud.com>
2018-02-14 11:42:56 +0530

    Experimenting with SnackBar and Coordinator layout

--- app/src/main/java/com/example/tanmay/shoppingapp/ProductPage.kt
+++ app/src/main/java/com/example/tanmay/shoppingapp/ProductPage.kt
@@ -2,10 +2,14 @@ package com.example.tanmay.shoppingapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
+import android.view.View
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_product_page.*
 
 class ProductPage : AppCompatActivity() {
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,26 @@ class ProductPage : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
+val coordinatorLayout : CoordinatorLayout = ProductPageCoordinatorLayout
+        val productName : TextView = ProductPageProductName
+        val productImage : ImageView = ProductPageProductImage
+        val productPrice : TextView = ProductPageProductPrice
+        val productDescription : TextView = ProductPageProductDescription
+        val seeMore : TextView = ProductPageDescriptionSeeMore
+        val quantitySeekBar : SeekBar = ProductPageProductQuantitySeekBar
+        val quantityTextView : TextView = ProductPageProductQuantityTextView
+        val buyButton : Button = ProductPageBuyButton
+        val addToCartButton : Button = ProductPageAddToCartButton
+
+
+        addToCartButton.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+                Snackbar.make(coordinatorLayout, "Added To Cart", Snackbar.LENGTH_SHORT).show()
+
+            }
+        })
 
     }
+
 }
