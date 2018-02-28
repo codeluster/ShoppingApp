09f909d8943994d13b739a61e83b771828bf970b
tanmay <tanmaysingal@icloud.com>
2018-02-28 23:51:12 +0530

    URI and Content Provider

--- app/src/main/java/com/example/tanmay/shoppingapp/DataSet/ProductListContract.java
+++ app/src/main/java/com/example/tanmay/shoppingapp/DataSet/ProductListContract.java
@@ -3,6 +3,7 @@ package com.example.tanmay.shoppingapp.DataSet;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 import com.example.tanmay.shoppingapp.Product;
@@ -14,10 +15,18 @@ import com.example.tanmay.shoppingapp.Product;
 public class ProductListContract {
 
 
+    public static final String CONTENT_AUTHORITY = "com.example.tanmay.shoppingapp";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+
     public static class ProductListPrimary implements BaseColumns {
 
         public static final String TABLE_NAME = "productListPrimary";
         public static final String COLUMN_NAME_PRODUCT_NAME = "name";
+
+        // public static final Uri PRODUCT_NAME_URI = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListPrimary.TABLE_NAME);
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListPrimary.TABLE_NAME);
+
         public static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " + ProductListPrimary.TABLE_NAME + " (" +
                         ProductListPrimary._ID + " INTEGER PRIMARY KEY," +
@@ -33,6 +42,11 @@ public class ProductListContract {
         public static final String TABLE_NAME = "productListMetaData";
         public static final String COLUMN_NAME_PRODUCT_PRICE = "price";
         public static final String COLUMN_NAME_PRODUCT_THUMBNAIL = "thumbnail";
+
+        // public static final Uri PRODUCT_PRICE_URI = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListMetaData.COLUMN_NAME_PRODUCT_PRICE);
+        //  public static final Uri PRODUCT_THUMBNAIL = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListMetaData.COLUMN_NAME_PRODUCT_THUMBNAIL);
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListMetaData.TABLE_NAME);
+
         public static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " +
                         ProductListMetaData.TABLE_NAME + " (" +
@@ -49,6 +63,10 @@ public class ProductListContract {
 
         public static final String TABLE_NAME = "productListProductImages";
         public static final String COLUMN_NAME_PRODUCT_IMAGE = "image";
+
+      //  public static final Uri PRODUCT_IMAGE = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListProductImages.COLUMN_NAME_PRODUCT_IMAGE);
+      public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, ProductListProductImages.TABLE_NAME);
+
         public static final String SQL_CREATE_ENTRIES =
                 "CREATE TABLE " +
                         ProductListProductImages.TABLE_NAME + " (" +
--- app/src/main/java/com/example/tanmay/shoppingapp/DataSet/ProductProvider.java
+++ app/src/main/java/com/example/tanmay/shoppingapp/DataSet/ProductProvider.java
@@ -2,10 +2,13 @@ package com.example.tanmay.shoppingapp.DataSet;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
+import com.example.tanmay.shoppingapp.ProductList;
 
 /**
  * Created by tanmay on 28/2/18.
@@ -13,9 +16,34 @@ import android.support.annotation.Nullable;
 
 public class ProductProvider extends ContentProvider {
 
+    ProductListContract.ProductReaderDbHelper productReaderDbHelper;
+
+    private static final int tableproductListPrimary = 1;
+    private static final int tableproddutcListMetaData = 2;
+    private static final int tableproductImages = 3;
+
+    private static final int tableproductListPrimary_ID = 4;
+    private static final int tableproddutcListMetaData_ID = 5;
+    private static final int tableproductImages_ID = 6;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListPrimary.TABLE_NAME, tableproductListPrimary);
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListMetaData.TABLE_NAME, tableproddutcListMetaData);
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListProductImages.TABLE_NAME, tableproductImages);
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListPrimary.TABLE_NAME + "/#", tableproductListPrimary_ID);
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListMetaData.TABLE_NAME + "/#", tableproddutcListMetaData_ID);
+        sUriMatcher.addURI(ProductListContract.CONTENT_AUTHORITY, ProductListContract.ProductListProductImages.TABLE_NAME + "/#", tableproductImages_ID);
+    }
+
     @Override
     public boolean onCreate() {
-        return false;
+
+        productReaderDbHelper = new ProductListContract.ProductReaderDbHelper(getContext(), null, null, 1);
+
+        return true;
     }
 
     @Nullable
