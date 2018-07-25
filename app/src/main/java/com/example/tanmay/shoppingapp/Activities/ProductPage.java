2081f30b81d9de6deee308283b34d346529728f9
tanmay <tanmaysingal@icloud.com>
2018-07-25 12:31:30 +0530

    idk

--- app/src/main/java/com/example/tanmay/shoppingapp/Activities/ProductPage.java
+++ app/src/main/java/com/example/tanmay/shoppingapp/Activities/ProductPage.java
@@ -1,7 +1,10 @@
-package com.example.tanmay.shoppingapp;
+package com.example.tanmay.shoppingapp.Activities;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -17,8 +20,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.tanmay.shoppingapp.Data.BaseContract;
+import com.example.tanmay.shoppingapp.R;
+import com.example.tanmay.shoppingapp.YourCart;
 
-public class ProductPage extends AppCompatActivity {
+public class ProductPage extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     TextView prodName;
     TextView prodPrice;
@@ -32,10 +37,6 @@ public class ProductPage extends AppCompatActivity {
     EditText quantityBox;
 
     int prodID;
-    int prodNameID;
-    int prodPriceID;
-    int prodDescID;
-    int prodImageID;
     int quantity;
     ImageView favourite;
     Boolean favourited;
@@ -69,17 +70,13 @@ public class ProductPage extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-
         finish();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.home_page_toolbar, menu);
-
         return true;
-
     }
 
     @Override
@@ -132,42 +129,13 @@ public class ProductPage extends AppCompatActivity {
         increase = findViewById(R.id.productPageQuantityIncrease);
         quantityBox = findViewById(R.id.productPageQuantity);
 
-        String[] projection = {
-
-                BaseContract.ProductEntry._ID,
-                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_THUMBNAIL,
-                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME,
-                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_PRICE,
-                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_IMAGE,
-                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_DESCRIPTION
-
-        };
-
         // A new Uri with the the row number appended as "/#" wildcard
         Uri xacv = Uri.withAppendedPath(BaseContract.ProductEntry.CONTENT_URI, getIntent().getStringExtra("itemClicked"));
-
-        //Selection and selectionArgs are null because they get overridden in the DataProvider's URI Matcher
-        Cursor cursor = getContentResolver().query(xacv, projection, null, null, null);
-
-        cursor.moveToFirst();
-
-        prodID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry._ID));
-        prodNameID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME));
-        prodPriceID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_PRICE));
-        prodDescID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_DESCRIPTION));
-        prodImageID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_IMAGE));
-
-        cursor.close();
 
         prodName = findViewById(R.id.ProductPageProductName);
         prodDesc = findViewById(R.id.ProductPageProductDescription);
         prodPrice = findViewById(R.id.ProductPageProductPrice);
         prodImage = findViewById(R.id.ProductPageProductImage);
-
-        prodName.setText(prodNameID);
-        prodDesc.setText(prodDescID);
-        prodPrice.setText(prodPriceID);
-        prodImage.setImageResource(prodImageID);
 
         //Expands description on clicking the description text
         prodDesc.setOnClickListener(new View.OnClickListener() {
@@ -209,12 +177,10 @@ public class ProductPage extends AppCompatActivity {
             public void onClick(View view) {
                 if (quantity < 30) {
                     quantity++;
-                    quantityBox.setText("" + quantity);
-                } else {
-
+                    quantityBox.setText(Integer.toString(quantity));
+                } else
                     Toast.makeText(ProductPage.this, "Cannot order more than 30 products", Toast.LENGTH_SHORT).show();
 
-                }
             }
         });
 
@@ -224,14 +190,68 @@ public class ProductPage extends AppCompatActivity {
                 if (quantity > 1) {
 
                     quantity--;
-                    quantityBox.setText("" + quantity);
+                    quantityBox.setText(Integer.toString(quantity));
 
-                } else {
-
+                } else
                     Toast.makeText(ProductPage.this, "Cannot order less than 1 product", Toast.LENGTH_SHORT).show();
-                }
+
             }
         });
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+        String[] projection = {
+                BaseContract.ProductEntry._ID,
+                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME,
+                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_PRICE,
+                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_DESCRIPTION,
+                BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_IMAGE
+        };
+
+        return new CursorLoader(this,
+                BaseContract.ProductEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null);
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+        // Prevent execution if new pet is to be added
+        if (cursor == null || cursor.getCount() < 1) return;
+
+        if (cursor.moveToFirst()) {
+
+            prodID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry._ID));
+            int prodNameID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_NAME));
+            int prodPriceID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_PRICE));
+            int prodDescID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_DESCRIPTION));
+            int prodImageID = cursor.getInt(cursor.getColumnIndexOrThrow(BaseContract.ProductEntry.COLUMN_NAME_PRODUCT_IMAGE));
+
+            prodName.setText(prodNameID);
+            prodDesc.setText(prodDescID);
+            prodPrice.setText(prodPriceID);
+            prodImage.setImageResource(prodImageID);
+
+
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // Reset the text fields
+        prodName.setText("");
+        prodDesc.setText("");
+        prodPrice.setText("");
+        // Set the image to the placeholder image
+        prodImage.setImageResource(R.drawable.placeholder);
     }
 
 }
